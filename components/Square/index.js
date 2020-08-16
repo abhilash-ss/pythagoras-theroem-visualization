dee09ad538a5bececa499089cf3cf745f6ae5878
itsmeAB <ss.abhilash3@gmail.com>
2020-08-16 20:40:39 +0530

    feat: remove sass and its dependencies

--- components/Square/index.js
+++ components/Square/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import styles from "./Square.module.scss";
 
 function Square({ size, type, opacity = 1 }) {
   const row = Array.from(Array(size).keys());
   const cellColorClass =
     type === "base" ? "square__cell--base" : "square__cell--altitude";
   return (
-    <div className={styles.square} style={{ opacity: opacity }}>
+    <div className="square" style={{ opacity: opacity }}>
       {row.map((i, rowIndex) => {
         return (
-          <div className={styles.square__row} key={rowIndex.toString()}>
+          <div className="square__row" key={rowIndex.toString()}>
             {row.map((i, colIndex) => (
               <div
                 key={colIndex.toString()}
-                className={styles.square__cell}
+                className="square__cell"
                 style={{
                   background:
                     type === "altitude" ||
@@ -27,6 +26,38 @@ function Square({ size, type, opacity = 1 }) {
           </div>
         );
       })}
+
+      <style jsx>{`
+        .square {
+          border: 4px solid black;
+        }
+
+        .square__row {
+          display: flex;
+          background: black;
+        }
+
+        .square__cell {
+          height: 40px;
+          width: 40px;
+          background: lightgrey;
+          margin: 1px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 45px;
+          font-weight: 800px;
+          color: white;
+        }
+
+        .square__cell--base {
+          color: red;
+        }
+
+        .square__cell--altitude {
+          color: blue;
+        }
+      `}</style>
     </div>
   );
 }
